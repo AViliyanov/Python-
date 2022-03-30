2b8e3efae655a79765c912d1c3195af3b3a9f0b4
AViliyanov <alexandervilyanov@gmail.com>
2022-03-30 13:49:06 +0300

    lesson3 delet on branch lesson4

--- lesson3/3_1.py
+++
@@ -1,9 +0,0 @@
-def argument(del_1, del_2):
-    try:
-        res = int(del_1) / int(del_2)
-        return res
-    except (ZeroDivisionError, ValueError) as err:
-        return err
-
-
-print(argument(input("Number_1:"), input("Number_2:")))
--- lesson3/3_4.py
+++
@@ -1,6 +0,0 @@
-def my_func(x, z):
-    res = abs(x) ** -abs(z)
-    return res
-
-
-print(my_func(float(input("x:")), int(input("z:"))))
